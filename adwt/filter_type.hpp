38e7d50a6efa48cb6324349cd5cf08de69b6f058
maxmarsc <maxime.coutant@grenoble-inp.org>
2023-10-19 00:32:21 +0200

    Added some filters types to benchmark

--- adwt/filter_type.hpp
+++ adwt/filter_type.hpp
@@ -14,6 +14,10 @@ namespace adwt {
 enum class FilterType {
   kType1,  // Butteworth order 2, cutoff at 0.45*samplerate
   kType2,  // Chebyshev type 2 order 10, cutoff at 0.61*samplerate
+
+  kType3,  // Butterworth order 4
+  kType4,  // Butterworth order 6
+  kType5,  // Chebyshev type 2 order 8
 };
 
 //==============================================================================
@@ -79,6 +83,50 @@ constexpr auto z<FilterType::kType2>() {
       -0.29931827 + 2.94764072I, -0.97441784 + 2.98286211I,
       -1.89003202 + 2.94796957I, -3.15580399 + 2.50801272I,
       -4.37780671 + 1.08149105I};
+}
+
+//==============================================================================
+// BT2 order 4
+template <>
+constexpr auto r<FilterType::kType3>() {
+  return std::array<std::complex<float>, 2>{-1.30610392 + 0.54100596I,
+                                            1.30610392 - 3.15321379I};
+}
+
+template <>
+constexpr auto z<FilterType::kType3>() {
+  return std::array<std::complex<float>, 2>{-1.08201191 + 2.61220784I,
+                                            -2.61220784 + 1.08201191I};
+}
+
+// BT2 order 6
+template <>
+constexpr auto r<FilterType::kType4>() {
+  return std::array<std::complex<float>, 3>{-4.30788702 - 8.10390177e-14I,
+                                            0.57714742 + 9.99648661e-01I,
+                                            3.73073959 - 6.46183052e+00I};
+}
+
+template <>
+constexpr auto z<FilterType::kType4>() {
+  return std::array<std::complex<float>, 3>{-1.99929732 + 1.99929732I,
+                                            -0.73179361 + 2.73109093I,
+                                            -2.73109093 + 0.73179361I};
+}
+
+// CH2 order 8
+template <>
+constexpr auto r<FilterType::kType5>() {
+  return std::array<std::complex<float>, 4>{
+      0.5995431 - 1.42710928e-03I, -0.10125199 + 2.87722419e+00I,
+      -7.73846024 - 1.56288290e+00I, 7.23293555 - 1.16751094e+01I};
+}
+
+template <>
+constexpr auto z<FilterType::kType5>() {
+  return std::array<std::complex<float>, 4>{
+      -0.37870934 + 2.57341531I, -1.23202254 + 2.49225066I,
+      -2.30871469 + 2.08510777I, -3.31413228 + 0.89104016I};
 }
 
 //==============================================================================
--- tests/benchmarks/adwt_benchmark.cpp
+++ tests/benchmarks/adwt_benchmark.cpp
@@ -58,7 +58,148 @@ void bmSweepUpOrder2(benchmark::State& state) {
 
       auto output_span = std::span(output_vec.begin() + i * block_size,
                                    output_vec.begin() + (i + 1) * block_size);
-      osc.process<adwt::Direction::kBidirectionnal>(phase_span, output_span);
+      osc.process<adwt::Direction::kForward>(phase_span, output_span);
+    }
+  }
+}
+
+void bmSweepUpOrder4(benchmark::State& state) {
+  const auto block_size       = state.range(0);
+  const auto num_frames       = 2048 * 100;
+  const auto num_blocks       = num_frames / block_size;
+  const auto samplerate       = 44100.F;
+  constexpr auto kSawWaveFile = std::string_view(ASSETS_DIR "/saw_2048.wav");
+
+  // Load the waveform file
+  auto waveform_sndfile   = SndfileHandle(kSawWaveFile.data(), SFM_READ);
+  const auto waveform_len = waveform_sndfile.frames();
+  auto waveform_vec       = std::vector<float>(waveform_len);
+  auto read = waveform_sndfile.readf(waveform_vec.data(), waveform_len);
+  if (read != waveform_len || waveform_len != 2048) {
+    std::cerr << "Failed to read waveform file" << std::endl;
+    std::abort();
+  }
+
+  // Init the waveform data
+  auto waveform_data =
+      adwt::WaveformData::build(waveform_vec, 1, static_cast<int>(samplerate));
+  if (waveform_data == nullptr) {
+    std::cerr << "Failed to build WaveformData object" << std::endl;
+    std::abort();
+  }
+
+  // Init the oscillator
+  auto osc = adwt::Oscillator<adwt::FilterType::kType3>{};
+  if (osc.init(std::move(waveform_data)) != 0) {
+    std::cerr << "Failed to init oscillator class" << std::endl;
+  }
+
+  // Init the sweep phase data
+  auto phase_sweep = std::vector<float>(num_frames);
+  generateLinearSweepPhase(phase_sweep, 20.F, samplerate / 2.F, samplerate);
+
+  // Prepare the processing block
+  auto block_dst = std::vector<float>(block_size);
+
+  for (auto _ : state) {
+    for (auto i : iter::range(num_blocks)) {
+      auto phase_span = std::span(phase_sweep.begin() + i * block_size,
+                                  phase_sweep.begin() + (i + 1) * block_size);
+      osc.process<adwt::Direction::kForward>(phase_span, block_dst);
+    }
+  }
+}
+
+void bmSweepUpOrder6(benchmark::State& state) {
+  const auto block_size       = state.range(0);
+  const auto num_frames       = 2048 * 100;
+  const auto num_blocks       = num_frames / block_size;
+  const auto samplerate       = 44100.F;
+  constexpr auto kSawWaveFile = std::string_view(ASSETS_DIR "/saw_2048.wav");
+
+  // Load the waveform file
+  auto waveform_sndfile   = SndfileHandle(kSawWaveFile.data(), SFM_READ);
+  const auto waveform_len = waveform_sndfile.frames();
+  auto waveform_vec       = std::vector<float>(waveform_len);
+  auto read = waveform_sndfile.readf(waveform_vec.data(), waveform_len);
+  if (read != waveform_len || waveform_len != 2048) {
+    std::cerr << "Failed to read waveform file" << std::endl;
+    std::abort();
+  }
+
+  // Init the waveform data
+  auto waveform_data =
+      adwt::WaveformData::build(waveform_vec, 1, static_cast<int>(samplerate));
+  if (waveform_data == nullptr) {
+    std::cerr << "Failed to build WaveformData object" << std::endl;
+    std::abort();
+  }
+
+  // Init the oscillator
+  auto osc = adwt::Oscillator<adwt::FilterType::kType4>{};
+  if (osc.init(std::move(waveform_data)) != 0) {
+    std::cerr << "Failed to init oscillator class" << std::endl;
+  }
+
+  // Init the sweep phase data
+  auto phase_sweep = std::vector<float>(num_frames);
+  generateLinearSweepPhase(phase_sweep, 20.F, samplerate / 2.F, samplerate);
+
+  // Prepare the processing block
+  auto block_dst = std::vector<float>(block_size);
+
+  for (auto _ : state) {
+    for (auto i : iter::range(num_blocks)) {
+      auto phase_span = std::span(phase_sweep.begin() + i * block_size,
+                                  phase_sweep.begin() + (i + 1) * block_size);
+      osc.process<adwt::Direction::kForward>(phase_span, block_dst);
+    }
+  }
+}
+
+void bmSweepUpOrder8(benchmark::State& state) {
+  const auto block_size       = state.range(0);
+  const auto num_frames       = 2048 * 100;
+  const auto num_blocks       = num_frames / block_size;
+  const auto samplerate       = 44100.F;
+  constexpr auto kSawWaveFile = std::string_view(ASSETS_DIR "/saw_2048.wav");
+
+  // Load the waveform file
+  auto waveform_sndfile   = SndfileHandle(kSawWaveFile.data(), SFM_READ);
+  const auto waveform_len = waveform_sndfile.frames();
+  auto waveform_vec       = std::vector<float>(waveform_len);
+  auto read = waveform_sndfile.readf(waveform_vec.data(), waveform_len);
+  if (read != waveform_len || waveform_len != 2048) {
+    std::cerr << "Failed to read waveform file" << std::endl;
+    std::abort();
+  }
+
+  // Init the waveform data
+  auto waveform_data =
+      adwt::WaveformData::build(waveform_vec, 1, static_cast<int>(samplerate));
+  if (waveform_data == nullptr) {
+    std::cerr << "Failed to build WaveformData object" << std::endl;
+    std::abort();
+  }
+
+  // Init the oscillator
+  auto osc = adwt::Oscillator<adwt::FilterType::kType5>{};
+  if (osc.init(std::move(waveform_data)) != 0) {
+    std::cerr << "Failed to init oscillator class" << std::endl;
+  }
+
+  // Init the sweep phase data
+  auto phase_sweep = std::vector<float>(num_frames);
+  generateLinearSweepPhase(phase_sweep, 20.F, samplerate / 2.F, samplerate);
+
+  // Prepare the processing block
+  auto block_dst = std::vector<float>(block_size);
+
+  for (auto _ : state) {
+    for (auto i : iter::range(num_blocks)) {
+      auto phase_span = std::span(phase_sweep.begin() + i * block_size,
+                                  phase_sweep.begin() + (i + 1) * block_size);
+      osc.process<adwt::Direction::kForward>(phase_span, block_dst);
     }
   }
 }
@@ -105,14 +246,19 @@ void bmSweepUpOrder10(benchmark::State& state) {
     for (auto i : iter::range(num_blocks)) {
       auto phase_span = std::span(phase_sweep.begin() + i * block_size,
                                   phase_sweep.begin() + (i + 1) * block_size);
-      osc.process<adwt::Direction::kBidirectionnal>(phase_span, block_dst);
+      osc.process<adwt::Direction::kForward>(phase_span, block_dst);
     }
   }
 }
 
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 BENCHMARK(bmSweepUpOrder2)->RangeMultiplier(2)->Range(16, 2048);
-// BENCHMARK(bmSweepUpOrder2)->Range(64, 64);
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+BENCHMARK(bmSweepUpOrder4)->RangeMultiplier(2)->Range(16, 2048);
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+BENCHMARK(bmSweepUpOrder6)->RangeMultiplier(2)->Range(16, 2048);
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+BENCHMARK(bmSweepUpOrder8)->RangeMultiplier(2)->Range(16, 2048);
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 BENCHMARK(bmSweepUpOrder10)->RangeMultiplier(2)->Range(16, 2048);
 
